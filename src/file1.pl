d71509f64bdf444f4904ef23ce74b81ad6ca1a24
Dylan1252 <124692275+Dylan1252@users.noreply.github.com>
2026-05-12 20:48:42 +0700

    feat : Fakta Dinamis, Fakta

--- src/file1.pl
+++ src/file1.pl
@@ -1,1 +1,39 @@
+% FAKTA DINAMIS		
+Jumlah pemain
 
+:- dynamic
+  pemain/1.
+	urutan/1.
+	hand/2. % menyimpan kartu yang dimiliki setiap pemain
+	discard_top/1.
+	giliran/1.
+
+% ========================================================
+% Fakta 
+% ========================================================
+
+% Warna Kartu
+warna(merah).
+warna(biru).
+warna(hijau).
+warna(kuning).
+warna(hitam).
+
+% Jenis Kartu Angka
+angka(0).
+angka(1).
+angka(2).
+angka(3).
+angka(4).
+angka(5).
+angka(6).
+angka(7).
+angka(8).
+angka(9).
+
+% Jenis Kartu Aksi
+aksi(skip).
+aksi(reverse). 
+aksi(ambil2).
+aksi(wild).
+aksi(wild_ambil4).
