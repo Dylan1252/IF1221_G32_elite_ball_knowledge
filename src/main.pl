9f6154dc9d94ac2b3e8fa99ce63ecaf1f233e026
Nesinez <13525114@std.stei.itb.ac.id>
2026-05-12 20:42:46 +0700

    Update main.pl

--- src/main.pl
+++ src/main.pl
@@ -1,1 +1,17 @@
+startGame :-
+  hapus_data,
 
+	write('Masukkan jumlah pemain: '),
+	read(Jumlah),
+	validasi(Jumlah),
+
+  input_pemain(1, Jumlah, DaftarPemain),
+
+  random_permutation(DaftarPemain, UrutanAcak),
+  assertz(urutan(UrutanAcak)),
+
+  write(‘Urutan pemain: ’), tampilkan_urutan(UrutanAcak), nl,
+
+  write(‘Setiap pemain mendapatkan 7 kartu acak.’),
+
+  write(‘Kartu discard top: ’), 
